e7b06274c845a71f3ff897c181e51692ddd3425a
sabbir-063 <202114063@student.mist.ac.bd>
2023-11-08 07:33:41 +0600

    kjn

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import { BiMessageSquareDetail } from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="/"><AiOutlineHome /></a>
+      <a href="/about"><AiOutlineUser /></a>
+      <a href="/services"><RiServiceLine /></a>
+      <a href="/experience"><BiBook /></a>
+      <a href="/contact"><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
